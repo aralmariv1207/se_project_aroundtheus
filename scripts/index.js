6bf495a294961f7110d73463bb62825ac2b48b21
Aldo Almanzar <ara4143@rit.edu>
2023-10-14 13:09:56 -0400

    Update: 'scripts/index.js' file

--- scripts/index.js
+++ scripts/index.js
@@ -30,7 +30,7 @@ const initialCards = [
   },
 ];
 
-const profileEditButton = document.querySelector("profile-edit-button");
+const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("profile-edit-modal");
 profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal_opened");
