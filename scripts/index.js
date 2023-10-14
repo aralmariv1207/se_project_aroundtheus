f17fbc1ffbd1536bd6b70053648cb0fcfdef3df2
Aldo Almanzar <ara4143@rit.edu>
2023-10-13 22:52:50 -0400

    Update: 'index.html' and 'scripts/index.js' files

--- scripts/index.js
+++ scripts/index.js
@@ -29,3 +29,10 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector("profile-edit-button");
+const profileEditModal = document.querySelector("profile-edit-modal");
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+  profileEditModal.classList.remove("modal_opened");
+});
