68ec902a77930508ab2644fa7e331c5fbb1cb798
Aldo Almanzar <ara4143@rit.edu>
2024-01-30 21:21:52 -0500

    Update: 'scripts/validate.js'

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,11 +1,31 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-enableValidation({
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(".popup__form")];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    // look for all inputs inside of form
+    // loop through all inputs to see if all are valid
+    // if input is not valid
+    // get validation message
+    // add error class to input
+    // display error message
+    // disable button
+    // if all inputs are valid
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
-});
+};
