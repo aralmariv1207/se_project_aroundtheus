4ae2e0755b99a120996eeb0a8e6739ac82a63445
Aldo Almanzar <ara4143@rit.edu>
2024-01-30 22:58:49 -0500

    Update: 'scripts/validate.js'

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,23 +1,63 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+function showInputError(formEl, inputEl, {inputErrorClass, errorClass});
+const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+inputEl.classList.add(inputErrorClass);
+errorMessageEl.textContent = inputEl.validationMessage;
+errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, {inputErrorClass, errorClass});
+const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+inputEl.classList.remove(inputErrorClass);
+errorMessageEl.textContent = '';
+errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+    if (!inputEl.validity.valid) {
+        return showInputError(formEl, inputEl, options);
+    }
+
+    hideInputError(formEl, inputEl, options);
+}
+
+function hasInvalidInput(inputList) {
+    return !inputList.every(inputEl) => inputEl.validity.valid);
+}
+
+// disableButton
+
+// enableButton
+
+function setEventListeners(formEl, options) {
+const {inputSelector} = options;
+const inputEls = [...formEl.querySelectorAll(inputSelector)]
+}
+
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(".popup__form")];
+  const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
 
+setEventListeners(formEl, options) {
+
+}
+
+
     // look for all inputs inside of form
     // loop through all inputs to see if all are valid
-    // if input is not valid
-    // get validation message
-    // add error class to input
-    // display error message
-    // disable button
+        // if input is not valid
+        // get validation message
+        // add error class to input
+        // display error message
+        // disable button
     // if all inputs are valid
-    // enable button
-    // reset error messages
+        // enable button
+        // reset error messages
   });
 }
 
