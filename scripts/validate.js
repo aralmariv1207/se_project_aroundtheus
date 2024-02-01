c6b1c354aee88e1fe2e070ff930f9fd8b9bff0ca
Aldo Almanzar <ara4143@rit.edu>
2024-02-01 00:18:58 -0500

    Update: 'scripts/validate.js'

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,17 +1,19 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
-function showInputError(formEl, inputEl, {inputErrorClass, errorClass})
-const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+function showInputError(formEl, inputEl, {inputErrorClass, errorClass}) {
+const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`)
 inputEl.classList.add(inputErrorClass);
 errorMessageEl.textContent = inputEl.validationMessage;
 errorMessageEl.classList.add(errorClass);
+}
 
-function hideInputError(formEl, inputEl, {inputErrorClass, errorClass});
-const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+function hideInputError(formEl, inputEl, {inputErrorClass, errorClass}) {
+const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`)
 inputEl.classList.remove(inputErrorClass);
 errorMessageEl.textContent = '';
 errorMessageEl.classList.remove(errorClass);
+}
 
 function checkInputValidity(formEl, inputEl, options) {
     if (!inputEl.validity.valid) {
@@ -22,7 +24,7 @@ function checkInputValidity(formEl, inputEl, options) {
 }
 
 function hasInvalidInput(inputList) {
-    return !inputList.every(inputEl) => inputEl.validity.valid);
+    return !inputList.every(inputEl) => (inputEl.validity.valid);
 }
 
 function toggleButtonState(inputEls, submitButton, {inactiveButtonClass}) {
