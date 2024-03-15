65642008b3046abc7c2e1079c6322b015a4be4d6
Aldo Almanzar <ara4143@rit.edu>
2024-03-15 01:43:50 -0400

    Update: 'scripts/validate.js' file

--- scripts/validate.js
+++ scripts/validate.js
@@ -78,12 +78,12 @@ function enableValidation(config) {
 }
 
 const config = {
-  formSelector: ".modal__form",
-  inputSelector: ".modal__input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonClass: ".modal__button_disabled",
-  inputErrorClass: ".modal__input_type_error",
-  errorClass: ".modal__error_visible",
+  formSelector: "modal__form",
+  inputSelector: "modal__input",
+  submitButtonSelector: "modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
 };
 
 enableValidation(config);
