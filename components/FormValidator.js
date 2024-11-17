073cec7a084b4b07d20799ba506b64e6982b273f
Aldo Almanzar <ara4143@rit.edu>
2024-11-17 14:57:14 -0500

    Update: 'components/FormValidator.js' file

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -71,6 +71,7 @@ export default class FormValidator {
     this._formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
+    this._setEventListeners();
   }
 
   resetValidation() {
