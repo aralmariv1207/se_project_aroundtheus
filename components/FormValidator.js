235c5fc3c82851e5e5d8fa1756dc3c77b0f5ed4a
Aldo Almanzar <ara4143@rit.edu>
2024-11-21 23:04:51 -0500

    Update: 'components/FormValidator.js' file

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -40,8 +40,7 @@ export default class FormValidator {
     // this.disableButton();
 
     if (this._hasInvalidInput()) {
-      this._submitButton.classList.add(this._inactiveButtonClass);
-      this._submitButton.disabled = true;
+      this.disableButton();
     } else {
       this._submitButton.classList.remove(this._inactiveButtonClass);
       this._submitButton.disabled = false;
