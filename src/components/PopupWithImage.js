e5184d4237b03f7a99e54613bb2a2f857adad578
Aldo Almanzar <ara4143@rit.edu>
2024-12-08 20:04:19 -0500

    Update: 'src/components/PopupWithImage.js' file

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,13 +3,13 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._imageElement = this._popup.classList.add(".image-preview-modal");
+    this._captionElement = this._popup.querySelector(".modal__caption");
   }
 
   open({ name, link }) {
-    this._popup.classList.add("#image-preview-modal");
     this._imageElement.src = link;
     this.imageElement.alt = name;
-
     this._captionElement.textContent = name;
     super.open();
   }
