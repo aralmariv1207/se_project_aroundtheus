d4edc64b2d240ae6c2424037de27ed241515bf1a
Aldo Almanzar <ara4143@rit.edu>
2024-12-16 19:55:52 -0500

    Update: 'src/components/Api.js' file

--- src/components/Api.js
+++ src/components/Api.js
@@ -24,23 +24,18 @@ export default class Api {
   }
   getUserInfo() {
     return fetch("https://around-api.en.tripleten-services.com/v1/users/me", {
+      method: "PATCH", 
       headers: {
         authorization: "89dc4b2f-fab0-42f3-ad8c-2593f7f5189c",
+        "Content-Type": "application/json"        
       },
+      body: JSON.stringify({
+        name: "Jacques Cousteau",
+        about: "Explorer"
+      })
     })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          return Promise.reject(`Error: ${res.status}`);
-        }
-      })
-      .then((data) => {
-        console.log(data);
-        return data;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
   }
-}
+} 
