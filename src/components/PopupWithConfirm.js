7471da522cf3d5678aaf34ac677cb41de04f5c0e
Aldo Almanzar <ara4143@rit.edu>
2024-12-21 15:58:58 -0500

    Update: 'PopupWithConfirm.js' file

--- src/components/PopupWithConfirm.js
+++ src/components/PopupWithConfirm.js
@@ -1,11 +1,23 @@
 import Popup from "./Popup";
 
 export default class PopupWithConfirm extends Popup {
-  constructor(popupSelector) {
+  constructor(popupSelector, handleConfirmModal) {
     super(popupSelector);
+    this._handleConfirmModal = handleConfirmModal;
+    this._form = this._popup.querySelector(".modal__form");
+    this._inputList = this._form.querySelectorAll(".modal__input");
   }
-  setSubmitFunction(submitFunction){}
-  setEventListeners(){
-    
+  setSubmitFunction(submitFnc){
+    this._submitFunction = submitFnc
+  }
+  
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      this._handleConfirmModal(this._getInputValues());
+    });
   }
 }
