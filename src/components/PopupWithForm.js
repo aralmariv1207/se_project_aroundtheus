e6f1326d835aeeb368c711ca21d2b1eec34dabd4
Aldo Almanzar <ara4143@rit.edu>
2025-01-02 14:19:42 -0500

    Update: 'PopupWithForm.js' file

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,7 +1,7 @@
 import Popup from "./Popup.js";
 
 export default class PopupWithForm extends Popup {
-  constructor(handleFormSubmit, popupSelector = ".modal") {
+  constructor(popupSelector, handleFormSubmit) {
     super(popupSelector);
     this._handleFormSubmit = handleFormSubmit;
 
